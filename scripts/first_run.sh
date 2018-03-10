833090177da009f65dc5e78aa980e78c971139bd
Ajeeth Samuel <ajeeth.samuel@gmail.com>
2018-03-10 23:27:23 +0400

    Update first_run.sh

--- scripts/first_run.sh
+++ scripts/first_run.sh
@@ -2,7 +2,8 @@
 
 sed -i -e "s/%URBACKUP_DOMAIN_NAME%/"$URBACKUP_DOMAIN_NAME"/g" /etc/nginx/sites-available/urbackup
 #dpkg -r urbackup-server
-dpkg -i /urbackup-server*.deb
+#dpkg -i /urbackup-server*.deb
+apt-get install -y /urbackup-server*.deb
 mkdir -p /media/BACKUP/urbackup
 #chmod -R 777 /media/BACKUP/urbackup
 #chown -R urbackup.urbackup /media/BACKUP/urbackup
